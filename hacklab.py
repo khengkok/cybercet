8da4da1765d509cef66aab770aae3bc4c16d630f
khengkok <khengkok@gmail.com>
2020-04-18 12:17:04 +0800

    added hacklab script

--- hacklab.py
+++ hacklab.py
@@ -3,9 +3,9 @@ import csv
 import argparse
 
 # Configuration:
-# pfsense with 2 interfaces in public and private subnet
-# win7#1 with 2 interfaces in public and private subnet
-# win7#2 with 1 interface in private subnet
+# Kali with 2 interfaces in public and private subnet
+# windows 10 (gateway) with 2 interfaces in public and private subnet
+# win7 with 1 interface in private subnet (victim machine)
 
 vpc = 'vpc_demo'
 subnet_public = 'subnet1_demo'
@@ -15,12 +15,15 @@ vpc_id = get_vpcId(vpc)
 public_subnetid = get_subnetId(vpc_id, subnet_public)
 private_subnetid  = get_subnetId(vpc_id,subnet_private)
 
+kali_ami_id = 'ami-08d5b1b0f5ed7e5f5'
+win7_gw_ami_id = 'ami-0535e8383179a6d1f'
+win7_victim_ami_id = 'ami-0a559734b59b0cb1c'
+
 def prov(num_instances, out_csvfile):
 
-    # pfsense 
-    pfsense_ami_id = 'ami-0077821e284c5f18e'
-    secgrpnames_public = ['pfsense_vpcdemo']  # public 
-    secgrpnames_private = ['pfsense_vpcdemo']  # private
+    # kali
+    secgrpnames_public = ['allow_ssh_vpc_demo']  # public 
+    secgrpnames_private = ['block_all_inbound_vpc_demo']  # private
 
     public_secgrpIdlist = get_secgrpIds(secgrpnames_public)
     private_secgrpIdlist = get_secgrpIds(secgrpnames_private)
@@ -35,14 +38,13 @@ def prov(num_instances, out_csvfile):
 
     subnet_secgrps_tuples = zip(subnetIdList, secgrpIdsList)
 
-    instanceIdList = create_instances(pfsense_ami_id, subnet_secgrps_tuples, num_instances, auto_assign_public_ip=True)
-    pfsense_infos = get_instances_info(instanceIdList)
-    print(pfsense_infos)
+    instanceIdList = create_instances(kali_ami_id, subnet_secgrps_tuples, num_instances, auto_assign_public_ip=True)
+    kali_infos = get_instances_info(instanceIdList)
+    print(kali_infos)
 
-    # win7#1 (public/private) 
-    win7_1_ami_id = 'ami-021ad573e15d1bf7d'
-    secgrpnames_public = ['win7rdp_vpc_demo_securitygroup']  # public 
-    secgrpnames_private = ['win7rdp_vpc_demo_securitygroup']  # private
+    # win7 gw (public/private) 
+    secgrpnames_public = ['allow_rdp_vpc_demo']  # public 
+    secgrpnames_private = ['block_all_inbound_vpc_demo']  # private
 
     public_secgrpIdlist = get_secgrpIds(secgrpnames_public)
     private_secgrpIdlist = get_secgrpIds(secgrpnames_private)
@@ -57,13 +59,12 @@ def prov(num_instances, out_csvfile):
 
     subnet_secgrps_tuples = zip(subnetIdList, secgrpIdsList)
 
-    instanceIdList = create_instances(win7_1_ami_id, subnet_secgrps_tuples, num_instances, auto_assign_public_ip=True)
-    win7_1_infos = get_instances_info(instanceIdList)
-    print(win7_1_infos)
+    instanceIdList = create_instances(win7_gw_ami_id, subnet_secgrps_tuples, num_instances, auto_assign_public_ip=True)
+    win7_gw_infos = get_instances_info(instanceIdList)
+    print(win7_gw_infos)
 
-    # win7#2 (private) 
-    win7_2_ami_id = 'ami-06be7fb41c22e3eaa'
-    secgrpnames_private = ['win7rdpsmbsecuritygroup']  # private
+    # win7 victim (private) 
+    secgrpnames_private = ['allow_rdp_smb_vpc_demo']  # private
 
     private_secgrpIdlist = get_secgrpIds(secgrpnames_private)
 
@@ -75,31 +76,31 @@ def prov(num_instances, out_csvfile):
 
     subnet_secgrps_tuples = zip(subnetIdList, secgrpIdsList)
 
-    instanceIdList = create_instances(win7_2_ami_id, subnet_secgrps_tuples, num_instances, auto_assign_public_ip=False)
-    win7_2_infos = get_instances_info(instanceIdList)
-    print(win7_2_infos)
+    instanceIdList = create_instances(win7_victim_ami_id, subnet_secgrps_tuples, num_instances, auto_assign_public_ip=False)
+    win7_victim_infos = get_instances_info(instanceIdList)
+    print(win7_victim_infos)
 
     # format csv
-    combined_infos = zip(pfsense_infos, win7_1_infos, win7_2_infos)
+    combined_infos = zip(kali_infos, win7_gw_infos, win7_victim_infos)
 
     info_dict_list = []
     
     for pfsense_info, win7_1_info, win7_2_info in combined_infos:
         info_dict = {}
-        info_dict['pfsense_instance_id'] = pfsense_info['id']
-        info_dict['pfsense_public_ip'] = pfsense_info['public_ip']
+        info_dict['kali_instance_id'] = pfsense_info['id']
+        info_dict['kali_public_ip'] = pfsense_info['public_ip']
         for intf in pfsense_info['nets']:
-            keyname = 'pfsense_priv_ip#' + str(intf['intf_index'])
+            keyname = 'kali_priv_ip#' + str(intf['intf_index'])
             info_dict[keyname] = intf['priv_ip']
-        info_dict['windows7_1_instance_id'] = win7_1_info['id']
-        info_dict['windows7_1_public_ip'] = win7_1_info['public_ip']
+        info_dict['win7_gw_instance_id'] = win7_1_info['id']
+        info_dict['win7_gw_public_ip'] = win7_1_info['public_ip']
         for intf in win7_1_info['nets']:
-            keyname = 'windows7_1_priv_ip#' + str(intf['intf_index'])
+            keyname = 'win7_gw_priv_ip#' + str(intf['intf_index'])
             info_dict[keyname] = intf['priv_ip']
-        info_dict['windows7_2_instance_id'] = win7_2_info['id']
+        info_dict['win7_victim_instance_id'] = win7_2_info['id']
         #info_dict['windows7_2_public_ip'] = win7_1_info['public_ip']
         for intf in win7_2_info['nets']:
-            keyname = 'windows7_2_priv_ip#' + str(intf['intf_index'])
+            keyname = 'win7_victim_priv_ip#' + str(intf['intf_index'])
             info_dict[keyname] = intf['priv_ip']
 
         info_dict_list.append(info_dict)
@@ -122,9 +123,9 @@ def deprov(csvfile):
         csv_reader = csv.DictReader(csv_file)
         instance_list = []
         for row in csv_reader:
-            instance_list.append(row['pfsense_instance_id'])
-            instance_list.append(row['windows7_1_instance_id'])
-            instance_list.append(row['windows7_2_instance_id'])
+            instance_list.append(row['kali_instance_id'])
+            instance_list.append(row['win7_gw_instance_id'])
+            instance_list.append(row['win7_victim_instance_id'])
         print('Deprovisioning {} instances'.format(len(instance_list)))
         delete_instances(instance_list)
 
